3afd2a21cbad5bc71d5574ad4bb07f861557b93f
Devkrishna <ambatikrishnasai1301@gmail.com>
2023-09-24 23:43:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isActive, setIsActive] = useState("");
+  const [winner, setWinner] = useState("");
+  const [computer, setComputer] = useState("");
+  const [select, setSelect] = useState(false);
+  const arr = ["rock", "paper", "Scissors"];
+
+  const checkWinner = (choice) => {
+    setComputer(arr[Math.floor(Math.random() * arr.length)]);
+    setIsActive(choice);
+
+    if (choice === computer) {
+      setWinner("It's a Tie");
+    } else if (
+      (choice === "rock" && computer === "paper") ||
+      (choice === "paper" && computer === "Scissors") ||
+      (choice === "Scissors" && computer === "rock")
+    ) {
+      setWinner("You Win");
+    } else {
+      setWinner("You Lose");
+    }
+    setSelect(true);
+  };
+
+  const reset = () => {
+    setWinner("");
+    setComputer("");
+    setSelect(false);
+    setIsActive("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <h2>RPC</h2>
+      <div className="box">
+        {select === false ? <p className="select">Select one option</p> : null}
+        <div>
+          <button
+            className={` ${isActive === "rock" ? "active" : ""}`}
+            onClick={() => checkWinner("rock")}
+          >
+            ROCK
+          </button>
+          <button
+            className={` ${isActive === "paper" ? "active" : ""}`}
+            onClick={() => checkWinner("paper")}
+          >
+            PAPER
+          </button>
+          <button
+            className={`] ${isActive === "Scissors" ? "active" : ""}`}
+            onClick={() => checkWinner("Scissors")}
+          >
+            SCISSORS
+          </button>
+        </div>
+        {select === true ? (
+          <>
+            <p className="select">{winner}</p>
+            <p className="select">Computer: {computer}</p>
+            <button className="bottom" onClick={() => reset()}>
+              Click to play
+            </button>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 }
